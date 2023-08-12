d4cdfd339f6b71411e07dc087044f06b00e920a5
Viktor Denisov <websitevirtuoso@gmail.com>
2023-08-11 19:00:57 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app>
+    <v-main>
+      Has to work even with disabled css utilities classes
+      <v-container>
+        <v-row>
+          <v-col md="6">
+            <v-text-field
+              label="First name"
+              hide-details="auto"
+            />
+          </v-col>
+          <v-col md="6">
+            <v-text-field
+              label="Last name"
+              hide-details="auto"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+      Using utility class d-flex to show that it exist
+      <div class="d-flex" style="gap: 20px">
+        <v-text-field
+          label="First name"
+          hide-details="auto"
+        />
+        <v-text-field
+          label="Last name"
+          hide-details="auto"
+        />
+      </div>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+</script>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,28 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+// import 'vuetify/styles' // by documentation commend it to use
+// https://vuetifyjs.com/en/features/sass-variables/#disabling-utility-classes
+import '@/settings.scss'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+})
